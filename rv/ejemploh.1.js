c14b7014f75707a2cddafbb3022495d1793e3cfb
ra0514 <ra.0514@hotmail.com>
2016-03-04 14:05:27 -0600

    Update ejemploh.1.js

--- rv/ejemploh.1.js
+++ rv/ejemploh.1.js
@@ -17,8 +17,8 @@ Pierna.prototype=new THREE.Object3D();
 
 function setup(){
 var cuerpo=new THREE.Mesh(new THREE.CylinderGeometry(1,2,5,10));
-piernaD=newPierna();
-piernaI=newPierna();
+piernaD=new Pierna();
+piernaI=new Pierna();
 cuerpo.position.y=2;
 piernaD.position.z=-1;
 piernaI.position.z=1;
