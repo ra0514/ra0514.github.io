2befbe20399f28d3bfab5e952ba176ae112cd405
ra0514 <ra.0514@hotmail.com>
2016-03-09 11:23:32 -0600

    Update meshlambermaterial_4.js

--- rv/meshlambermaterial_4.js
+++ rv/meshlambermaterial_4.js
@@ -3,7 +3,7 @@ var forma=new THREE.BoxGeometry(1,1,1);
 var material=new THREE.MeshLambertMaterial({color:'#ffffff'});
 malla=new THREE.Mesh(forma,material);
 
-var LuzPuntual=new THREE.PointLight(0xcc00cc);
+var luzPuntual=new THREE.PointLight(0xcc00cc);
 luzPuntual.position.x=10;
 luzPuntual.position.y=10;
 luzPuntual.position.z=10;
@@ -29,7 +29,7 @@ malla.rotation.y+=0.01;
 renderer.render(escena,camara);
 }
 
-var camara,escenar,renderer,malla,luzPuntual;
+var camara,escenar,renderer,malla;
 
 setup();
 loop();
