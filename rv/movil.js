2796cd29e3f061665010f76b198fe15aebe60907
ra0514 <ra.0514@hotmail.com>
2016-03-08 22:34:50 -0600

    Update movil.js

--- rv/movil.js
+++ rv/movil.js
@@ -32,8 +32,8 @@ escena.add(llanta2);
 
 camara=new THREE.PerspectiveCamera();
 camara.position.z=20;
-camara.position.x=5;
-camara.position.y=10;
+camara.position.x=2;
+camara.position.y=5;
 
 renderer=new THREE.WebGLRenderer();
 renderer.setSize(window.innerHeight*.95,window.innerHeight*.95);
