4ec683a151cfa4b23c6a53c709b411fe897709a7
ra0514 <ra.0514@hotmail.com>
2016-03-16 11:02:10 -0600

    Create textura_5.js

---
+++ rv/textura_5.js
@@ -0,0 +1,39 @@
+
+function setup(){
+THREE.ImageUtils.crossOrigin='';
+var textura = THREE.ImageUtils.loadTexture('http://threejs.org/examples/textures/planets/earth_atmos_2048.jpg');
+var material=new THREE.MeshPhongMaterial({map:textura});
+var forma=new THREE.SphereGeometry(1);
+malla=new THREE.Mesh(forma,material);
+
+malla.scale.set(1,4,9);
+
+var LuzPuntual=new THREE.PointLight(0xFFFFFF);
+LuzPuntual.position.x=10;
+LuzPuntual.position.y=10;
+LuzPuntual.position.z=10;
+
+escena=new THREE.Scene();
+escena.add(malla);
+escena.add(LuzPuntual);
+
+camara=new THREE.PerspectiveCamera();
+camara.position.z=10;
+
+renderer=new THREE.WebGLRenderer();
+renderer.setSize(window.innerHeight*.95,window.innerHeight*.95);
+document.body.appendChild(renderer.domElement);
+}
+
+function loop(){
+requestAnimationFrame(loop);
+
+malla.rotation.x +=0.01;
+malla.rotation.y +=0.01;
+
+renderer.render(escena,camara);
+}
+
+var camara,excena,renderer,malla;
+setup();
+loop();
