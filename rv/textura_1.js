9008ef2c456989d39507e93da19a44bd8a38e587
ra0514 <ra.0514@hotmail.com>
2016-03-16 10:27:49 -0600

    Update textura_1.js

--- rv/textura_1.js
+++ rv/textura_1.js
@@ -3,7 +3,7 @@ THREE.ImageUtils.crossOrigin='';
 var textura = THREE.ImageUtils.loadTexture('http://threejs.org/examples/textures/crate.gif');
 var material=new THREE.MeshBasicMaterial({map:textura});
 var forma=new THREE.BoxGeometry(1,1,1);
-malla=new THREE.Mesh(forma,materail);
+malla=new THREE.Mesh(forma,material);
 
 escena=new THREE.Scene();
 escena.add(malla);
