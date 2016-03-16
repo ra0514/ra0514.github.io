95e00832747f067289d603b01f38243280252595
ra0514 <ra.0514@hotmail.com>
2016-03-16 10:56:03 -0600

    Update textura_4.js

--- rv/textura_4.js
+++ rv/textura_4.js
@@ -1,8 +1,8 @@
 
 function setup(){
 THREE.ImageUtils.crossOrigin='';
-var textura = THREE.ImageUtils.loadTexture('http://threejs.org/examples/textures/brick_diffuse.jpg');
-var material=new THREE.MeshLambertMaterial({map:textura});
+var textura = THREE.ImageUtils.loadTexture('http://threejs.org/examples/textures/cube/pisa(nz.png');
+var material=new THREE.MeshPhongMaterial({map:textura});
 var forma=new THREE.BoxGeometry(1,1,1);
 malla=new THREE.Mesh(forma,material);
 malla.scale.set(1,4,9);
