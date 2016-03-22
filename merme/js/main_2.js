1d79d5ad2a23cf89042deb054e446d0700c1ec12
ra0514 <ra.0514@hotmail.com>
2016-03-21 19:43:57 -0600

    Update main_2.js

--- merme/js/main_2.js
+++ merme/js/main_2.js
@@ -1,35 +1,64 @@
-function setup(){
-var material=new THREE.MeshBasicMaterial();
+var scene, camera, renderer;
 
-var gl = canvas.getContext('webgl');
-var objStr = document.getElementById('my_cube.obj').innerHTML;
-var mesh = new OBJ.Mesh(objStr);
+var WIDTH  = window.innerWidth;
+var HEIGHT = window.innerHeight;
 
+var SPEED = 0.01;
 
-var forma=new THREE.BoxGeometry(1,1,1);
+function init() {
+    scene = new THREE.Scene();
 
-malla=new THREE.Mesh(forma,material);
+    initMesh();
+    initCamera();
+    initLights();
+    initRenderer();
 
-escena=new THREE.Scene();
-escena.add(malla);
-
-camara=new THREE.PerspectiveCamera();
-camara.position.z=100;
-
-renderer=new THREE.WebGLRenderer();
-renderer.setSize(window.innerHeight*.95,window.innerHeight*.95);
-document.body.appendChild(renderer.domElement);
+    document.body.appendChild(renderer.domElement);
 }
 
-function loop(){
-requestAnimationFrame(loop);
-
-malla.rotation.x +=0.01;
-malla.rotation.y +=0.01;
-
-renderer.render(escena,camara);
+function initCamera() {
+    camera = new THREE.PerspectiveCamera(70, WIDTH / HEIGHT, 1, 10);
+    camera.position.set(0, 3.5, 5);
+    camera.lookAt(scene.position);
 }
 
-var camara,excena,renderer,malla;
-setup();
-loop();
+
+function initRenderer() {
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize(WIDTH, HEIGHT);
+}
+
+function initLights() {
+    var light = new THREE.AmbientLight(0xffffff);
+    scene.add(light);
+}
+
+var mesh = null;
+function initMesh() {
+    var loader = new THREE.JSONLoader();
+    loader.load('./marmelab.json', function(geometry, materials) {
+        mesh = new THREE.Mesh(geometry, new THREE.MeshFaceMaterial(materials));
+        mesh.scale.x = mesh.scale.y = mesh.scale.z = 0.75;
+        mesh.translation = THREE.GeometryUtils.center(geometry);
+        scene.add(mesh);
+    });
+}
+
+function rotateMesh() {
+    if (!mesh) {
+        return;
+    }
+
+    mesh.rotation.x -= SPEED * 2;
+    mesh.rotation.y -= SPEED;
+    mesh.rotation.z -= SPEED * 3;
+}
+
+function render() {
+    requestAnimationFrame(render);
+    rotateMesh();
+    renderer.render(scene, camera);
+}
+
+init();
+render();
