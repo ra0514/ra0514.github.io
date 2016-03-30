3794d5c3cf46f20edabb9b57debc867bf0795179
ra0514 <ra.0514@hotmail.com>
2016-03-30 11:14:21 -0600

    Update colision_2.js

--- rv/colision_2.js
+++ rv/colision_2.js
@@ -21,7 +21,7 @@ escena.add(camara);
 renderer=new THREE.WebGLRenderer();
 renderer.setSize(window.innerHeight*.95,window.innerHeight*.95);
 document.body.appendChild(renderer.domElement);
-step=0.05;
+step=0.2;
 }
 
 function loop(){
