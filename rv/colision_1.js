f35fbaf9de33838b57480bd09ce53d483e95f758
ra0514 <ra.0514@hotmail.com>
2016-03-30 10:38:42 -0600

    Update colision_1.js

--- rv/colision_1.js
+++ rv/colision_1.js
@@ -5,7 +5,7 @@ cubo2=new THREE.Mesh(new THREE.BoxGeometry(1,1,1),new THREE.MeshNormalMaterial()
 cubo1.position.x=0.7;
 cubo2.position.x=-0.7;
 
-camara=new THREE.PerspectivrCamera();
+camara=new THREE.PerspectiveCamera();
 camara.position.z=5;
 
 raycaster=new THREE.Raycaster();
