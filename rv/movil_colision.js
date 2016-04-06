09b21bfa999fcccb87c8b0909b4a532cc15e9331
ra0514 <ra.0514@hotmail.com>
2016-04-06 11:04:04 -0500

    Update movil_colision.js

--- rv/movil_colision.js
+++ rv/movil_colision.js
@@ -11,7 +11,6 @@ pared2.position.y=-30;
 pared3.position.x=30;
 pared4.position.x=-30;
 
-
 var canon=new THREE.Mesh(new THREE.BoxGeometry(4,4,1));
 var sujetador=new THREE.Mesh(new THREE.BoxGeometry(1,6,1));
 
@@ -54,14 +53,39 @@ camara.position.z=80;
 camara.position.x=2;
 camara.position.y=5;
 
+
+raycaster1=new THREE.Raycaster(pelota.position,new THREE.Vector3(9,0,0));
+raycaster2=new THREE.Raycaster(pelota.position,new THREE.Vector3(-9,0,0));
+raycaster3=new THREE.Raycaster(pelota.position,new THREE.Vector3(0,9,0));
+raycaster4=new THREE.Raycaster(pelota.position,new THREE.Vector3(0,-9,0));
+
 renderer=new THREE.WebGLRenderer();
 renderer.setSize(window.innerHeight*.95,window.innerHeight*.95);
 document.body.appendChild(renderer.domElement);
+step=0.5;
 }
 
-function loop(){
-requestAnimationFrame(loop);
 
+
+function loop(){
+obstaculo1=raycaster1.intersectObject(cubo1);
+obstaculo2=raycaster2.intersectObject(cubo2);
+obstaculo3=raycaster3.intersectObject(cubo3);
+obstaculo4=raycaster4.intersectObject(cubo4);
+  
+  if((obstaculo1.length>0 && (obstaculo1[0].distance<=0.5))||(obstaculo2.length>0 && (obstaculo2[0].distance<=0.5)))
+step=-step;
+malla.position.y +=step;
+
+
+raycaster1.set(malla.position,new THREE.Vector3(9,0,0));
+raycaster2.set(malla.position,new THREE.Vector3(-9,0,0));
+raycaster3.set(malla.position,new THREE.Vector3(0,9,0));
+raycaster4.set(malla.position,new THREE.Vector3(0,-9,0));
+  
+  
+  
+requestAnimationFrame(loop);
 renderer.render(escena,camara);
 }
 
