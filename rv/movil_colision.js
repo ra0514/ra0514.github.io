c9dc3e75a74f7489504f840ee6d9aab90115e487
ra0514 <ra.0514@hotmail.com>
2016-04-06 10:47:49 -0500

    Create movil_colision.js

---
+++ rv/movil_colision.js
@@ -0,0 +1,75 @@
+function setup(){
+
+//cuarto
+pared1=new THREE.Mesh(new THREE.BoxGeometry(1,1,1),new THREE.MeshNormalMaterial());
+pared2=new THREE.Mesh(new THREE.BoxGeometry(1,1,1),new THREE.MeshNormalMaterial());
+pared3=new THREE.Mesh(new THREE.BoxGeometry(1,1,1),new THREE.MeshNormalMaterial());
+pared4=new THREE.Mesh(new THREE.BoxGeometry(1,1,1),new THREE.MeshNormalMaterial());
+
+pared1.rotation.z=20;
+pared2.rotation.z=-20;
+pared3.position.x=20;
+pared4.position.x=-20;
+
+
+var canon=new THREE.Mesh(new THREE.BoxGeometry(4,4,1));
+var sujetador=new THREE.Mesh(new THREE.BoxGeometry(1,6,1));
+
+var abajo=new THREE.Mesh(new THREE.BoxGeometry(6,6,1));
+var llanta1=new THREE.Mesh(new THREE.CylinderGeometry(2,2,1,10));
+var llanta2=new THREE.Mesh(new THREE.CylinderGeometry(2,2,1,10));
+
+canon.position.y=0;
+sujetador.position.y=3;
+
+abajo.position.z=-1;
+llanta1.rotation.z=1.57;
+llanta2.rotation.z=1.57;
+llanta1.position.z=-1;
+llanta2.position.z=-1;
+llanta1.position.x=-3.5;
+llanta2.position.x=3.5;
+
+var forma=new THREE.Geometry();
+
+THREE.GeometryUtils.merge(forma,canon);
+THREE.GeometryUtils.merge(forma,sujetador);
+THREE.GeometryUtils.merge(forma,abajo);
+THREE.GeometryUtils.merge(forma,llanta1);
+THREE.GeometryUtils.merge(forma,llanta2);
+
+malla=new THREE.Mesh(forma);
+
+escena=new THREE.Scene();
+escena.add(malla);
+
+//agregar cuarto
+escena.add(pared1);
+escena.add(pared2);
+escena.add(pared3);
+escena.add(pared4);
+
+camara=new THREE.PerspectiveCamera();
+camara.position.z=50;
+camara.position.x=2;
+camara.position.y=5;
+
+renderer=new THREE.WebGLRenderer();
+renderer.setSize(window.innerHeight*.95,window.innerHeight*.95);
+document.body.appendChild(renderer.domElement);
+}
+
+function loop(){
+requestAnimationFrame(loop);
+
+malla.rotation.x+=0.01;
+malla.rotation.y+=0.01;
+
+renderer.render(escena,camara);
+}
+
+var escena,camara,renderer;
+var malla;
+
+setup();
+loop();
