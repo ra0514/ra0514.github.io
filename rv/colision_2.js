18bdf49b4790e2605965e7c6a89fd241fd4ec5a9
ra0514 <ra.0514@hotmail.com>
2016-04-06 20:27:19 -0500

    Update colision_2.js

--- rv/colision_2.js
+++ rv/colision_2.js
@@ -1,7 +1,7 @@
 function setup(){
 cubo1=new THREE.Mesh(new THREE.BoxGeometry(1,1,1),new THREE.MeshNormalMaterial());
 cubo2=new THREE.Mesh(new THREE.BoxGeometry(1,1,1),new THREE.MeshNormalMaterial());
-pelota=new THREE.Mesh(new THREE.SphereGeometry(2.5),new THREE.MeshNormalMaterial());
+pelota=new THREE.Mesh(new THREE.SphereGeometry(0.5),new THREE.MeshNormalMaterial());
 
 cubo1.position.x=7;
 cubo2.position.x=-7
