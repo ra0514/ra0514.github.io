7be58df8bff48e2fe59e01f6968ede68707f22cf
ra0514 <ra.0514@hotmail.com>
2016-04-06 20:57:06 -0500

    Update movil_colision.js

--- rv/movil_colision.js
+++ rv/movil_colision.js
@@ -1,9 +1,9 @@
 function setup(){
-cubo1=new THREE.Mesh(new THREE.BoxGeometry(1,60,1),new THREE.MeshNormalMaterial());
-cubo2=new THREE.Mesh(new THREE.BoxGeometry(1,60,1),new THREE.MeshNormalMaterial());
+cubo1=new THREE.Mesh(new THREE.BoxGeometry(1,40,1),new THREE.MeshNormalMaterial());
+cubo2=new THREE.Mesh(new THREE.BoxGeometry(1,40,1),new THREE.MeshNormalMaterial());
 
-cubo3=new THREE.Mesh(new THREE.BoxGeometry(1,1,1),new THREE.MeshNormalMaterial());
-cubo4=new THREE.Mesh(new THREE.BoxGeometry(1,1,1),new THREE.MeshNormalMaterial());
+cubo3=new THREE.Mesh(new THREE.BoxGeometry(40,1,1),new THREE.MeshNormalMaterial());
+cubo4=new THREE.Mesh(new THREE.BoxGeometry(40,1,1),new THREE.MeshNormalMaterial());
 
 var canon=new THREE.Mesh(new THREE.BoxGeometry(4,4,1));
 var sujetador=new THREE.Mesh(new THREE.BoxGeometry(1,6,1));
@@ -45,14 +45,16 @@ camara.position.z=60;
 raycaster1=new THREE.Raycaster(pelota.position,new THREE.Vector3(1,0,0));
 raycaster2=new THREE.Raycaster(pelota.position,new THREE.Vector3(-1,0,0));
 
+raycaster3=new THREE.Raycaster(pelota.position,new THREE.Vector3(0,1,0));
+raycaster4=new THREE.Raycaster(pelota.position,new THREE.Vector3(0,-1,0));
+
 escena=new THREE.Scene();
 escena.add(cubo1);
 escena.add(cubo2);
-escena.add(pelota);
-escena.add(camara);
-
 escena.add(cubo3);
 escena.add(cubo4);
+escena.add(pelota);
+escena.add(camara);
 
 renderer=new THREE.WebGLRenderer();
 renderer.setSize(window.innerHeight*.95,window.innerHeight*.95);
@@ -64,6 +66,8 @@ function loop(){
 
 obstaculo1=raycaster1.intersectObject(cubo1);
 obstaculo2=raycaster2.intersectObject(cubo2);
+obstaculo3=raycaster3.intersectObject(cubo3);
+obstaculo4=raycaster4.intersectObject(cubo4);
 
 if((obstaculo1.length>0 && (obstaculo1[0].distance<=4))||(obstaculo2.length>0 && (obstaculo2[0].distance<=4)))
 step=-step;
