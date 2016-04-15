b5bc8dfce4ddd2a77feeeff0c6d0ea12b8687b3e
ra0514 <ra.0514@hotmail.com>
2016-04-15 10:43:14 -0500

    Update movil_agentes.js

--- rv/movil_agentes.js
+++ rv/movil_agentes.js
@@ -1,24 +1,24 @@
 function Pelota(x=0,y=0){
 
 //var canon=new THREE.Mesh(new THREE.BoxGeometry(4,4,1));
-var sujetador=new THREE.Mesh(new THREE.BoxGeometry(1,6,1));
+//var sujetador=new THREE.Mesh(new THREE.BoxGeometry(1,6,1));
 var abajo=new THREE.Mesh(new THREE.BoxGeometry(6,6,1));
 var llanta1=new THREE.Mesh(new THREE.CylinderGeometry(2,2,1,10));
 var llanta2=new THREE.Mesh(new THREE.CylinderGeometry(2,2,1,10));
 
 //canon.position.y=0;
-sujetador.position.y=3;
-abajo.position.z=-1;
+//sujetador.position.y=3;
+//abajo.position.z=-1;
 llanta1.rotation.z=1.57;
 llanta2.rotation.z=1.57;
-llanta1.position.z=-1;
-llanta2.position.z=-1;
+//llanta1.position.z=-1;
+//llanta2.position.z=-1;
 llanta1.position.x=-3.5;
 llanta2.position.x=3.5;
 
 var forma=new THREE.Geometry();
 //THREE.GeometryUtils.merge(forma,canon);
-THREE.GeometryUtils.merge(forma,sujetador);
+//THREE.GeometryUtils.merge(forma,sujetador);
 THREE.GeometryUtils.merge(forma,abajo);
 THREE.GeometryUtils.merge(forma,llanta1);
 THREE.GeometryUtils.merge(forma,llanta2);
