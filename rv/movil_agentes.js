2625ea56fdafc44f33eba6873a08376913857c4e
ra0514 <ra.0514@hotmail.com>
2016-04-15 09:50:36 -0500

    Update movil_agentes.js

--- rv/movil_agentes.js
+++ rv/movil_agentes.js
@@ -44,7 +44,7 @@ Pelota.prototype.sense=function(enviroment){
   this.sensor.set(this.position,new THREE.Vector3(0,-1,0));
   var obstaculo2=this.sensor.intersectObjects(enviroment.children,true);
   
-  if((obstaculo1.length>0 && (obstaculo1[0].distance <= 1)) || (obstaculo2.length>0 && (obstaculo2[0].distance <= 5)))
+  if((obstaculo1.length>0 && (obstaculo1[0].distance <= 2)) || (obstaculo2.length>0 && (obstaculo2[0].distance <= 20)))
   this.colision=1;
   else
   this.colision=0;
