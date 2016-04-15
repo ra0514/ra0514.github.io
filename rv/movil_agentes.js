267124d0b39a37ab8cece889a672e9e1712aeba5
ra0514 <ra.0514@hotmail.com>
2016-04-15 11:16:31 -0500

    Update movil_agentes.js

--- rv/movil_agentes.js
+++ rv/movil_agentes.js
@@ -51,18 +51,18 @@ Pelota.prototype.sense=function(enviroment){
   this.stepx=0.2;
   } 
   
-  if(obstaculo1.length>0 && (obstaculo1[0].distance <= 1))
+  if(obstaculo1.length>0 && (obstaculo1[0].distance <= 2))
   {
   this.stepy=-0.2;
   this.stepx=0;
   } 
-  /*
+
   if(obstaculo4.length>0 && (obstaculo4[0].distance <= 3))
   {
   this.stepy=0;
   this.stepx=-0.2;
   } 
-  
+    /*
   if(obstaculo2.length>0 && (obstaculo2[0].distance <= 3)&&(obstaculo3[0].distance >= 3))
   {
   this.stepy=0.2;
