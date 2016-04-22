3d5a2badfcad246e9e28e6ac1fe9ae5a889214d0
ra0514 <ra.0514@hotmail.com>
2016-04-22 13:26:27 -0500

    Update movil_luz_e_1.js

--- rv/movil_luz_e_1.js
+++ rv/movil_luz_e_1.js
@@ -76,6 +76,7 @@ if((obstaculo3.length>0 && (obstaculo3[0].distance<=5)))
 {stepy=0;
 pelota.rotation.z=-1.57;
 stepx=0.2;
+material1=material2;
 };
 
 if((obstaculo1.length>0 && (obstaculo1[0].distance<=5)))
