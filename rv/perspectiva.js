e58d0dfb36cd02a85ad7145fea5950a511ae2b9e
ra0514 <ra.0514@hotmail.com>
2016-05-11 10:26:03 -0500

    Update perspectiva.js

--- rv/perspectiva.js
+++ rv/perspectiva.js
@@ -2,7 +2,7 @@ function setup(){
 THREE.ImageUtils.crossOrigin='';
 var textura = THREE.ImageUtils.loadTexture('mosaico.jpg');
 var material=new THREE.MeshBasicMaterial({map:textura});
-var forma=new THREE.BoxGeometry(1,1,1);
+var forma=new THREE.BoxGeometry(1,1,0.1);
 malla=new THREE.Mesh(forma,material);
 
 escena=new THREE.Scene();
@@ -11,6 +11,9 @@ escena.add(malla);
 camara=new THREE.PerspectiveCamera();
 camara.position.z=5;
 
+camara2 = new THREE.OrthographicCamera( 5 / - 2, 5 / 2, 5 / 2, 5 / - 2, 1, 1000 );
+camara2.position.z=5;
+
 renderer=new THREE.WebGLRenderer();
 renderer.setSize(window.innerHeight*.95,window.innerHeight*.95);
 document.body.appendChild(renderer.domElement);
@@ -18,9 +21,9 @@ document.body.appendChild(renderer.domElement);
 
 function loop(){
 requestAnimationFrame(loop);
-renderer.render(escena,camara);
+renderer.render(escena,camara2);
 }
 
-var camara,excena,renderer,malla;
+var camara,camara2,escena,renderer,malla;
 setup();
 loop();
