cf6b9413c211a6cd1b9cfed29ff391c445d936b8
ra0514 <ra.0514@hotmail.com>
2016-05-10 22:24:09 -0500

    Update perspectiva.js

--- rv/perspectiva.js
+++ rv/perspectiva.js
@@ -2,7 +2,7 @@ function setup(){
 THREE.ImageUtils.crossOrigin='';
 var textura = THREE.ImageUtils.loadTexture('http://www.clairvision.org/ckb/ckbr/ckbi/fol_0000_0001/cat_0000_0001/imagfile_0000_0000_0000_2000_0000_0000_0220_0085.gif');
 var material=new THREE.MeshBasicMaterial({map:textura});
-var forma=new THREE.BoxGeometry(10,10,1);
+var forma=new THREE.BoxGeometry(1,1,1);
 malla=new THREE.Mesh(forma,material);
 
 escena=new THREE.Scene();
@@ -18,10 +18,6 @@ document.body.appendChild(renderer.domElement);
 
 function loop(){
 requestAnimationFrame(loop);
-
-malla.rotation.x +=0.01;
-malla.rotation.y +=0.01;
-
 renderer.render(escena,camara);
 }
 
