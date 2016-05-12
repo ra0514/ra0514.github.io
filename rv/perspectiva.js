b551f03c4b0520e3e155d8c91c71ca9fa01317d7
ra0514 <ra.0514@hotmail.com>
2016-05-12 18:20:11 -0500

    Update perspectiva.js

--- rv/perspectiva.js
+++ rv/perspectiva.js
@@ -2,7 +2,8 @@ var keyboard = new THREEx.KeyboardState();
 
 function setup(){
 THREE.ImageUtils.crossOrigin='';
-var textura = THREE.ImageUtils.loadTexture('mosaico.jpg');
+//var textura = THREE.ImageUtils.loadTexture('mosaico.jpg');
+var textura = THREE.ImageUtils.loadTexture('http://threejs.org/examples/textures/crate.gif');
 var material=new THREE.MeshBasicMaterial({map:textura});
 var forma=new THREE.BoxGeometry(1,1,1);
 malla=new THREE.Mesh(forma,material);
