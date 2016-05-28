187b737bab9d1a17e85dca6913c2a5ba940b590a
ra0514 <ra.0514@hotmail.com>
2016-05-28 13:03:37 -0500

    Update campos.js

--- rv/campos.js
+++ rv/campos.js
@@ -4,7 +4,7 @@ THREE.ImageUtils.crossOrigin='';
 var textura = THREE.ImageUtils.loadTexture('http://threejs.org/examples/textures/brick_diffuse.jpg');
 
 //la habitacion  
-cubo1=new THREE.Mesh(new THREE.BoxGeometry(0.5,60,3),new THREE.MeshBasicMaterial({map:textura}));
+cubo1=new THREE.Mesh(new THREE.BoxGeometry(0.5,60,3));
 cubo2=new THREE.Mesh(new THREE.BoxGeometry(0.5,60,3),new THREE.MeshBasicMaterial({map:textura}));
 cubo3=new THREE.Mesh(new THREE.BoxGeometry(60,0.5,3),new  THREE.MeshBasicMaterial({map:textura}));
 cubo4=new THREE.Mesh(new THREE.BoxGeometry(60,0.5,3),new  THREE.MeshBasicMaterial({map:textura}));
